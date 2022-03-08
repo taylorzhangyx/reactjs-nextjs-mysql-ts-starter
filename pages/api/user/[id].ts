52f0e90c1a87074f1bf295c02fb4dfe348564f0b
root <tayzhang@tencent.com>
2022-03-08 23:02:16 +0800

    add typeorm
    Signed-off-by: root <tayzhang@tencent.com>

---
+++ pages/api/user/[id].ts
@@ -0,0 +1,47 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { userRepo } from "../../../repositories/userRepo";
+
+type User = {
+  id: number;
+  name: string;
+};
+
+const sampleUserData: User[] = [
+  { id: 101, name: "Alice" },
+  { id: 102, name: "Bob" },
+  { id: 103, name: "Caroline" },
+  { id: 104, name: "Dave" },
+];
+
+const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const {
+      query: { id, name },
+      body: users,
+      method,
+    } = _req;
+
+    if (Array.isArray(id) || Array.isArray(name)) {
+      throw new Error("only take id and name as string not array");
+    }
+
+    switch (method) {
+      case "GET":
+        // Get data from your database
+        const user = await userRepo.getOne(id);
+        res.status(200).json(user);
+        break;
+      case "POST":
+        // Update or create data in your database
+        res.status(200).json({ result: "success" });
+        break;
+      default:
+        res.setHeader("Allow", ["GET", "POST"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } catch (err: any) {
+    res.status(500).json({ statusCode: 500, message: err.message });
+  }
+};
+
+export default handler;
---
+++ pages/api/users.ts
@@ -0,0 +1,45 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type User = {
+  id: number;
+  name: string;
+};
+
+const sampleUserData: User[] = [
+  { id: 101, name: "Alice" },
+  { id: 102, name: "Bob" },
+  { id: 103, name: "Caroline" },
+  { id: 104, name: "Dave" },
+];
+
+const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const {
+      query: { id, name },
+      body: users,
+      method,
+    } = _req;
+
+    if (!Array.isArray(sampleUserData)) {
+      throw new Error("Cannot find user data");
+    }
+
+    switch (method) {
+      case "GET":
+        // Get data from your database
+        res.status(200).json(sampleUserData);
+        break;
+      case "POST":
+        // Update or create data in your database
+        res.status(200).json({ result: "success" });
+        break;
+      default:
+        res.setHeader("Allow", ["GET", "POST"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } catch (err: any) {
+    res.status(500).json({ statusCode: 500, message: err.message });
+  }
+};
+
+export default handler;
